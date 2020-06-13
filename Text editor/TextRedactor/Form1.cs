119765a016d3f293b73b6ae13511288af167d45a
valerchik666 <60138705+valerchik666@users.noreply.github.com>
2020-06-13 13:09:26 +0300

    Add files via upload

---
+++ Text editor/TextRedactor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordPadMin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog opfd = new OpenFileDialog();
+            SaveFileDialog sfd = new SaveFileDialog();
+            opfd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            sfd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfd = new OpenFileDialog();
+            if (opfd.ShowDialog() == DialogResult.OK) ;
+            
+                string filename = opfd.FileName;
+                string fileText = System.IO.File.ReadAllText(filename, Encoding.GetEncoding(1251));
+                richTextBox1.Text = fileText;
+            
+        }
+        
+        private bool showdialog = true;
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            string filename = sfd.FileName;
+
+                
+                
+            if (showdialog || filename.Length == 0)
+            {
+                
+                sfd.FileName = richTextBox1.Text;
+                sfd.FileName = "Unnamed";
+                sfd.Filter = "Text files(*.txt)|*.txt|RTF (*.rtf)|*.rtf";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    filename = sfd.FileName;
+                    showdialog = false;
+                }
+                else
+                    return;
+            }
+            System.IO.File.WriteAllText(filename, richTextBox1.Text);
+            MessageBox.Show("Файл сохранен");
+            
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
